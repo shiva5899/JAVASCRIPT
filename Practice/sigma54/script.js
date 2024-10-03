fe84f7f0b868dee6baac593f2163c79f55d7a2bd
Shivam Khonde <shivamkhonde@gmail.com>
2024-10-03 18:08:58 +0530

    Started JS

---
+++ Practice/sigma54/script.js
@@ -0,0 +1,22 @@
+alert("Hello World");
+
+console.log("Code is running");
+console.log("Code is also running");
+console.log("Code is looking like a wow");
+
+var a = prompt("Enter you number");
+console.log("Your number is " + a);
+
+var isTrue = confirm("You sure want to leave the page and blast your computer?");
+if(isTrue)
+{
+    console.log("Computer is blasting");
+}
+else{
+    console.log("Computer is not blasting");
+}
+
+document.title = "Hello bro!!";
+
+// Adds Inline css
+document.body.style.backgroundColor = "red";
