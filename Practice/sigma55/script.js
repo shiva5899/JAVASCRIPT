1429537f8c205293e05f594e0620fe59b6869e67
Shivam Khonde <shivamkhonde@gmail.com>
2024-10-04 22:06:05 +0530

    JS Variables

---
+++ Practice/sigma55/script.js
@@ -0,0 +1,38 @@
+console.log("This is my 2nd JS tutorial");
+
+var a = 20;
+var b = 10;
+console.log(a + b + 8);
+
+var c = "string"; 
+console.log(typeof a, typeof b, typeof c);
+
+var _a = "Shub";
+// var 5f = "Naman"; Invalid
+
+// JS is case-sensitive
+
+// var is global scope
+// let is blocked scope
+
+let t = "hello";
+let y = 22;
+let r = 4.56;
+let z = undefined;
+let m = true;
+let o = null;
+
+console.log(t, y, r, z, m, o);
+console.log(typeof t, typeof y, typeof r, typeof z, typeof m, typeof o);
+// null is of type object, its an old mistake by JS makers
+
+let l = {
+    "names": "jcnljadn",
+    "job code": 54345,
+    "is_handsome": true
+}
+console.log(l);
+
+l.salary = "3000k"
+console.log(l)
+
